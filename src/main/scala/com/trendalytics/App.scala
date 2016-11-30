84637bc7f5491bc794d713726837288b993edadd
pdhar <pranav.dhar2@gmail.com>
2016-11-30 18:39:46 -0500

    stop words cleaning added

--- src/main/scala/com/trendalytics/App.scala
+++ src/main/scala/com/trendalytics/App.scala
@@ -25,7 +25,7 @@ object App {
   
   def main(args : Array[String]) {
 
-    // val sc = new SparkContext(new SparkConf().setAppName("Trendalytics"))
+    val sc = new SparkContext(new SparkConf().setAppName("Trendalytics"))
 
     //val twitter = new TwitterFilter()
    // twitter.fetch()
@@ -43,11 +43,28 @@ object App {
     val hdfsObj = new HDFSManager()
 
     hdfsObj.createFolder("trendalytics_data")
+    hdfsObj.createFolder("trendalytics_data/tweets")
+    hdfsObj.createFolder("trendalytics_data/tweets_processed")
 
     val yelpOutputFile = "trendalytics_data/output.csv"
 
     if(!hdfsObj.isFilePresent(yelpOutputFile))
         hdfsObj.saveFile(yelpOutputFile)
+
+    val tweetFile = "trendalytics_data/tweets/20161130_060833.txt"
+
+    if(!hdfsObj.isFilePresent(tweetFile))
+        hdfsObj.saveFile(tweetFile)
+
+    val text = sc.textFile(tweetFile)
+    val stopWords = sc.textFile("trendalytics_data/stop_words.txt")
+    val words = text.flatMap(line => line.split("\\W"))
+    val clean = words.subtract(stopwords)
+
+    val cleanTweetPath = "trendalytics_data/tweets_processed/20161130_060833.txt"
+
+    if(!hdfsObj.isFilePresent(cleanTweetPath))
+        clean.saveAsTextFile(cleanTweetPath)
 
     return
 
