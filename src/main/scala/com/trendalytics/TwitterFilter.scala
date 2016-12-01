143bf3fb217e3fea2165f38908d2d8405cc024bf
Guangyu Zhang <guangyu.zhang@CONAV008.DN.ED.NYU.EDU>
2016-12-01 01:08:10 -0500

    Filter tweets by keywords

--- src/main/scala/com/trendalytics/TwitterFilter.scala
+++ src/main/scala/com/trendalytics/TwitterFilter.scala
@@ -1,6 +1,7 @@
 package com.trendalytics
 
 import java.io._
+import scala.io.Source
 import org.apache.http.client._
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.client.utils.URIBuilder
@@ -18,39 +19,38 @@ import java.text._
  */
 object FilterUtil {
   val config = new twitter4j.conf.ConfigurationBuilder()
-  .setOAuthConsumerKey("OycBERb5BcBhDj9b0HkUrkCM2")
-  .setOAuthConsumerSecret("Cg7tyvJF90zLq9MmOk2gvzLF0SSbIA8yF26ITNF9cgulSejBFy")
-  .setOAuthAccessToken("278043834-NTTee6inRqUg2cBvEl1NpazpsskDHQZ8N8cUww8j")
-  .setOAuthAccessTokenSecret("z4LpUMLYvuEKDe5H23LBlPQbdF7aaz3H6kerl56CjR8UP")
+  .setOAuthConsumerKey("ADEo7Xxrq7Gz0uF0rdkc4pdEY")
+  .setOAuthConsumerSecret("yDvWWeEP4WLAgkexEOD79Xp9dOT16pjX2phmQ0t2BR2ihbff8d")
+  .setOAuthAccessToken("804203820532252672-2xp6JVWFepbN6lmYnxPLXUwLUUTEgQO")
+  .setOAuthAccessTokenSecret("tcXQpznt4za1RqJa4mYNB57lFDIcCwUM8lCzhq7fCOtAf")
   .setUseSSL(true)
   .build
-}
 
-class TwitterFilter {
-  def fetch() {
-
+  def storeTweets (key_name: String) = {
     val twitter = new TwitterFactory(FilterUtil.config).getInstance
-    
+
+    val query = new Query(key_name)
+
     val long = -74.1687;
     val lat = 40.5722;
     val res = 50;
     val resUnit = "mi";
-
-    val query = new Query("apple")
-
     // query.setGeoCode(new GeoLocation(lat, lon), res, resUnit);
+    query.setLang("en");    // restrict to English tweets
     val result = twitter.search(query)
 
-    println("Tweetes Found :" + result.getTweets.size)
+    println('\t' + key_name + ":\t" + result.getTweets.size)
 
     val statuses = result.getTweets()
 
     val it = statuses.iterator()
 
     val dNow = new Date()
-    val ft = new SimpleDateFormat ("yyyy.MM.dd_hh:mm:ss")
+    // val ft = new SimpleDateFormat ("yyyy.MM.dd_hh:mm:ss")
+    val ft = new SimpleDateFormat ("MMddyyyy_hh")
 
-    val currentTime = ft.format(dNow).toString().replaceAll("[-+.^:,]","");
+    // val currentTime = ft.format(dNow).toString().replaceAll("[-+.^:,]","");
+    val currentTime = ft.format(dNow).toString();
 
     while (it.hasNext()) {
       val status = it.next()
@@ -60,17 +60,45 @@ class TwitterFilter {
       val numFriends = status.getUser.getFriendsCount.toString()
       val datetime = status.getCreatedAt.toString()
       val location  = status.getGeoLocation()
-      val tweets = status.getText
+      val tweets = status.getText.replaceAll("[\n\t]",". ");
       val numReTweet = status.getRetweetCount.toString()
 
       val pw = new FileWriter("trendalytics_data/tweets/" + currentTime + ".txt", true)
       val delimiter = "\t"
-      val toPrint = id + delimiter + userName + delimiter + numFriends + delimiter + datetime + delimiter + tweets + delimiter + numReTweet
+      val toPrint = key_name + delimiter + tweets + delimiter + id + delimiter + userName + delimiter + numReTweet + delimiter + numFriends + delimiter + datetime
 
       pw.write(toPrint + "\n")
 
       pw.close
     }
+  }
+
+  def filterKeyTweets (filename: String, idx: Int) = {
+    try {
+        val bufferedSource = Source.fromFile(filename)
+        for (line <- bufferedSource.getLines) {
+            val lines = line.toLowerCase.split('\t')
+            storeTweets(lines(idx));
+        }
+        bufferedSource.close
+    } catch {
+        case e: FileNotFoundException => println("Can't read from file " + filename)
+    }
+  }
+}
+
+class TwitterFilter {
+  def fetch() {    
+
+    println("------- Begin to search for MOVIES -------");
+    println("Number of Tweets found for:");
+    FilterUtil.filterKeyTweets("trendalytics_data/movies.txt", 0);
+    println("Finished searching for movies.\n");
+
+    println("------- Begin to search for RESTAURANTS -------");
+    println("Number of Tweets found for:");
+    FilterUtil.filterKeyTweets("trendalytics_data/yelp20LinesClean.txt", 1);
+    println("Finished searching for restaurants.\n");
 
   }
 }
