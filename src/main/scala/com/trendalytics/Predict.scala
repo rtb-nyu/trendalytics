22825846849befb4e43291f7568fe498aec4b405
Twitter Query <gabriellezgy@gmail.com>
2016-12-05 19:24:03 -0500

    load trained model for prediction

--- src/main/scala/com/trendalytics/Predict.scala
+++ src/main/scala/com/trendalytics/Predict.scala
@@ -17,13 +17,12 @@ import org.apache.hadoop.fs.FileSystem
 import org.apache.hadoop.fs.Path
 import java.net.URI
 // import org.apache.hadoop.util.Progressable
-
-import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.sql.SQLContext._
 
 import org.apache.spark.mllib.feature.HashingTF
 import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
 import org.apache.spark.mllib.clustering.KMeans
 
 // Import Row.
@@ -35,7 +34,7 @@ import org.apache.spark.sql.types.{StructType,StructField,StringType};
 /**
  * @author ${user.name}
  */
-object App {
+object Predict {
   
   // case class TweetRecord(key_name: String, text: String, id: String, username: String, retweets: Int, num_friends: Int, datetime: String)
   val numFeatures = 1000
@@ -50,30 +49,18 @@ object App {
       }
   }
 
-  def removeStopwords(sc : SparkContext, sqlContext : SQLContext, selectedData : DataFrame) : DataFrame = {
-    val sw = new StopWordFilter()
-    sw.remove(sc, sqlContext, selectedData)
-  }
-
   def featurize(s: String): Vector = {
     tf.transform(s.sliding(2).toSeq)
   }
 
   def main(args : Array[String]) {
 
+    println("Initializing Streaming Spark Context...")
+    val conf = new SparkConf().setAppName(this.getClass.getSimpleName)
+    // val ssc = new StreamingContext(conf, Seconds(5))
+
+
     val sc = new SparkContext(new SparkConf().setAppName("Trendalytics"))
-
-    val twitter = new TwitterFilter()
-    twitter.fetch()
-
-     // val facebook = new FacebookStreamer()
-     // facebook.fetch()
-
-    // val tmdb = new TMDBStreamer()
-    // tmdb.fetch()
-
-    // val yelp = new YelpStreamer()
-    // yelp.fetch()
 
     val hdfsObj = new HDFSManager()
 
@@ -130,38 +117,45 @@ object App {
 
     df.select(df("key"), df("text")).show()
 
-    val filteredData = removeStopwords(sc, sqlContext, selectedData)
-    // println("------The filteredData with stop words removed in tweets-----")
-    // filteredData.show()
-
-    filteredData.registerTempTable("tweets_filtered")
-
-    val texts = sqlContext.sql("SELECT filtered_text from tweets_filtered").map(t => t(0).toString)
-    // Caches the vectors since it will be used many times by KMeans.
-    val vectors = texts.map(featurize).cache()
-    vectors.count()  // Calls an action to create the cache.
+    val texts = sqlContext.sql("SELECT text from tweets").map(t => t(0).toString)
 
     val numIterations = 10
     val numClusters = 5
+    val modelFile = "trendalytics_data/tweets_processed/KMeansModel"
 
-    val model = KMeans.train(vectors, numClusters, numIterations)
+    // write predicted clustering tweets into files
+    // val dNow = new Date()
+    // val ft = new SimpleDateFormat ("MMddyyyy_hh")
+    // val currentTime = ft.format(dNow).toString();
+    val pwKmeans = new FileWriter("trendalytics_data/clusters/" + numClusters.toString + "-" + numIterations.toString + "_KMeans_predict.txt", true)
 
-    hdfsObj.deleteFolder("trendalytics_data/tweets_processed")
+    // Caches the vectors since it will be used many times by KMeans.
+    val vectors = texts.map(featurize).cache()
+    pwKmeans.write("Vectors Count: " + vectors.count().toString() + "\n")  // Calls an action to create the cache.
+
+    // val model = KMeans.train(vectors, numClusters, numIterations)
+
+    // hdfsObj.deleteFolder("trendalytics_data/tweets_processed")
     
-    // sc.makeRDD(model.clusterCenters, numClusters).saveAsObjectFile("trendalytics_data/tweets_processed/KMeansModel")
-    model.save(sc, "trendalytics_data/tweets_processed/KMeansModel")
+    // sc.makeRDD(model.clusterCenters, numClusters).saveAsObjectFile("trendalytics_data/tweets_processed")
+    println("Initalizaing the the KMeans model...")
+    // val model = new KMeansModel(ssc.sparkContext.objectFile[Vector](modelFile.toString).collect())
+    val model = KMeansModel.load(sc, modelFile)
 
     val some_tweets = texts.take(100)
     println("----Example tweets from the clusters")
     for (i <- 0 until numClusters) {
-      println(s"\nCLUSTER $i:")
+      // println(s"\nCLUSTER $i:")
+      pwKmeans.write(s"\nCLUSTER $i:\n")
       some_tweets.foreach { t =>
         if (model.predict(featurize(t)) == i) {
-          println(t)
+          // println(t)
+          pwKmeans.write(t.toString + "\n")
         }
       }
     }
-filteredData.show()
+    pwKmeans.close
+
     return
 
   }
