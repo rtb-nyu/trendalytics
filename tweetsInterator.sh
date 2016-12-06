fc9177c61022653efefe362cfb19ce67d25ce2a6
Twitter Query <gabriellezgy@gmail.com>
2016-12-05 23:31:24 -0500

    more tweets for training

--- tweetsInterator.sh
+++ tweetsInterator.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # Loop to fetch realtime tweets
 
-for i in {1..10}
+for i in {1..15}
 do
 	echo "-----------------------------------"
 	echo "###### Begin "$i" interation ######"
 	echo "-----------------------------------"
-	scala -cp target/trendalytics-0.0.1.jar com.trendalytics.App
-	printf "Pause to wait for new tweets...\n" 
+    # scala -cp target/trendalytics-0.0.1.jar com.trendalytics.App
+    spark-submit --class com.trendalytics.App --master yarn target/trendalytics-0.0.1.jar
+	printf "Pause to wait for new tweets...\n"
 	sleep 15m
 done
