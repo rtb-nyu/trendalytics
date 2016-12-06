77b7761b7018b83c83d9e3778ffa012c7c7c0fca
Twitter Query <gabriellezgy@gmail.com>
2016-12-05 23:54:14 -0500

    store jar files for collecting tweets

--- tweetsInterator.sh
+++ tweetsInterator.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # Loop to fetch realtime tweets
 
-for i in {1..15}
+for i in {1..20}
 do
 	echo "-----------------------------------"
 	echo "###### Begin "$i" interation ######"
 	echo "-----------------------------------"
     # scala -cp target/trendalytics-0.0.1.jar com.trendalytics.App
-    spark-submit --class com.trendalytics.App --master yarn target/trendalytics-0.0.1.jar
+    spark-submit --class com.trendalytics.App --master yarn trendalytics_data/trendalytics-fetchMoviesOnly.jar
 	printf "Pause to wait for new tweets...\n"
 	sleep 15m
 done
+
+# spark-submit --class com.trendalytics.App --master yarn trendalytics_data/trendalytics-fetchMoviesRestaurants.jar
