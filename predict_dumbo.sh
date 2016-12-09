220d680810e85849f1ae29fea93c7d32c9e93a69
Twitter Query <gabriellezgy@gmail.com>
2016-12-09 17:18:04 -0500

    add log files

--- predict_dumbo.sh
+++ predict_dumbo.sh
@@ -1,7 +1,9 @@
 /opt/maven/bin/mvn clean
-/opt/maven/bin/mvn package
+/opt/maven/bin/mvn package 2>&1 | tee outputResults/logFiles/predict_maven_build_jar.log
 
-spark-submit --class com.trendalytics.Predict --master yarn target/trendalytics-0.0.1.jar
+spark-submit --class com.trendalytics.Predict \
+--master yarn target/trendalytics-0.0.1.jar \
+2>&1 | tee outputResults/logFiles/predict_realtimeTweets.log
 
 # /user/<net id>/book.txt /user/<net id>/output
 # mvn package exec:java -Dexec.mainClass=com.trendalytics.App
